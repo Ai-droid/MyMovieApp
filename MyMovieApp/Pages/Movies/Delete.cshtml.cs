2a8da43f0d6788214c023ef6f7bc738eff5ad2d8
Ai-droid <abhinavsharma01jsr@gmail.com>
2020-10-01 22:09:59 +0530

    Add project files.

---
+++ MyMovieApp/Pages/Movies/Delete.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MyMovieApp.Data;
+using MyMovieApp.Models;
+
+namespace MyMovieApp
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly MyMovieApp.Data.MyMovieAppContext _context;
+
+        public DeleteModel(MyMovieApp.Data.MyMovieAppContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Movies Movies { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Movies = await _context.Movies.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Movies == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Movies = await _context.Movies.FindAsync(id);
+
+            if (Movies != null)
+            {
+                _context.Movies.Remove(Movies);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ MyMovieApp/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MyMovieApp.Data;
+using MyMovieApp.Models;
+
+namespace MyMovieApp
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MyMovieApp.Data.MyMovieAppContext _context;
+
+        public IndexModel(MyMovieApp.Data.MyMovieAppContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movies> Movies{ get; set; }
+
+        public async Task OnGetAsync()
+        {
+            List<Models.Movies> lists = await _context.Movies.ToListAsync();
+            Movies = (IList<Movies>)lists;
+        }
+    }
+}
